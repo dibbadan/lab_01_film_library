4192fb13ec7a2ab4f8bab134ce0c420e58909a55
Daniele Di Battista <100209953+dibbadan@users.noreply.github.com>
2022-03-10 11:15:09 +0100

    Delete src directory

--- src/warm-up.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let strings = ['Hello', 'B', 'World', 'it', 'cat', 'Javascript', 'Spring'];
-
-strings.map((string) => {
-    string.length < 2 ? console.log('') : console.log(string.slice(0,2).concat(string.slice(string.length-2,string.length)));
-})
